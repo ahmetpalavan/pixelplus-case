344e9724f70a6c19b05105642a5b36bc90e7773f
ahmetpalavan <ahmetpalavann@gmail.com>
2023-11-15 12:15:11 +0300

    Refactor Home component to use custom hooks and update UI
    - Refactored the import statements for styles and components
    - Removed unused interfaces
    - Replaced useState with custom hook useUsers to fetch user data
    - Added loading state and error handling logic
    - Updated Header component props
    - Added subheader section with Create New Member button functionality using useModalStore hook
    - Updated mapping of user data in cardsContainer div
    - Updated Pagination component prop for totalPages

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,47 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import UserCard from "@/components/UserCard";
-import styles from "@/app/variables.module.scss";
+import styles from "@/app/styles/variables.module.scss";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Loading from "@/components/Loading";
 import Pagination from "@/components/Pagination";
-interface User {
-  id: number;
-  email: string;
-  first_name: string;
-  last_name: string;
-  avatar: string;
-}
-
-interface ApiResponse {
-  page: number;
-  total_pages: number;
-  data: User[];
-}
+import UserCard from "@/components/UserCard";
+import { useModalStore } from "@/hooks/useModalStore";
+import { useUsers } from "@/hooks/useUsers";
+import React, { useState } from "react";
 
 const Home: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [totalPages, setTotalPages] = useState<number>(0);
+  const { data, isError, error, isLoading } = useUsers(currentPage);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await fetch(`https://reqres.in/api/users?page=${currentPage}`);
-        const data: ApiResponse = await response.json();
-        setUsers(data.data);
-        setTotalPages(data.total_pages);
-      } catch (error) {
-        console.error("Fetching users failed", error);
-      }
-    };
-
-    fetchUsers();
-  }, [currentPage]);
+  const openModal = useModalStore((state) => state.onOpen);
 
   const handlePageChange = (pageNumber: number) => {
     setCurrentPage(pageNumber);
   };
 
+  if (isLoading) {
+    return <Loading size={150} color="#123abc" />;
+  }
+  if (isError && error) return <div>Error: {error.message}</div>;
+
   return (
     <div className={styles.main}>
-      <Header />
+      <Header title="Pixelplus Case Study" />
       <section className={styles.mainContent}>
-        <h2 className={styles.membersTitle}>All Members</h2>
-        <button className={styles.createMemberButton}>Create New Member</button>
+        <section className={styles.subheaderContent}>
+          <h2 className={styles.membersTitle}>All Members</h2>
+          <button  onClick={() => openModal("createUserModal")}
+          className={styles.button}>Create New Member</button>
+        </section>
         <div className={styles.cardsContainer}>
-          {users.map((user) => (
+          {data?.data.map((user) => (
             <UserCard key={user.id} user={user} />
           ))}
         </div>
       </section>
-      <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+      <Pagination currentPage={currentPage} totalPages={data?.total_pages || 0} onPageChange={handlePageChange} />
+      <Footer />
     </div>
   );
 };
